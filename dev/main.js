027ad3bf801c50dc194dae6f8f6cea7f7eba7388
Amy Cheng <amy.cheng@brooklynmuseum.org>
2014-01-23 16:53:28 -0500

    appending wikipedia content

--- dev/main.js
+++ dev/main.js
@@ -1,16 +1,23 @@
 (function($){
- var socket = io.connect('http://localhost');
- var $list = $('#changes');
- socket.on('message', function (data) {
-  if ($list.find('li').length>0) {
-    $list.find('li:first').before('<li>'+data.title+'</li>');
-  }else{
-   $list.append('<li>'+data.title+'</li>');
 
- };
-});
+  var socket = io.connect('http://localhost');
+  var $list = $('#changes');
+  var $contentSidebar = $('#wikicontent');
+
+  socket.on('entry', function (data) {
+    if ($list.find('li').length>0) {
+      $list.find('li:first').before('<li>'+data.title+'</li>');
+    }else{
+      $list.append('<li>'+data.title+'</li>');
+    }
+  });
+  socket.on('wiki_content', function (data){
+    $contentSidebar.find('h2').text(data.title);
+    $contentSidebar.find('img').attr('src',data.image);
+  });
   // $( document ).ready(function() {
   //   console.log("running main.js");
 
   // });
 }(jQuery));
+
