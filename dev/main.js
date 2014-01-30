cbb59a16485116ae24d1e99557714ab7339a3f96
Amy Cheng <amy.cheng@brooklynmuseum.org>
2014-01-30 15:00:34 -0500

    did some minor clean-up

--- dev/main.js
+++ dev/main.js
@@ -1,22 +1,22 @@
 (function($){
 
-var minWidth= 480; //mobile in portrait
-var pageTotal= 0;
-var renderText = function(){
-  $('.title-wrapper').flowtype({
-    minimum : minWidth,
-    fontRatio: 20,
-    lineRatio: 2
-  });
-  $('.copy-wrapper').flowtype({
-    minimum : minWidth,
-    fontRatio: 28
-  });
-  $('.timestamp,.page-count,.stream').flowtype({
-    minimum: 650,
-    maximum:  730
-  });
-};
+  var renderText = function(){
+    var minWidth= 480; //mobile in portrait
+    var pageTotal= 0;
+    $('.title-wrapper').flowtype({
+      minimum : minWidth,
+      fontRatio: 20,
+      lineRatio: 2
+    });
+    $('.copy-wrapper').flowtype({
+      minimum : minWidth,
+      fontRatio: 28
+    });
+    $('.timestamp,.page-count,.stream').flowtype({
+      minimum: 650,
+      maximum:  730
+    });
+  };
 
   //socket.io stuff
   var socket = io.connect('http://localhost');
@@ -40,13 +40,9 @@ var renderText = function(){
     $('#js-timestamp').text(data.timestamp);
   });
 
-$( document ).ready(function() {
-  console.log('running scripts');
-  renderText();
-
-});
-// if there are 12 list items, removes the last one before appending the first one
-// append most recent timestamp
-// append page count
+  $( document ).ready(function() {
+    console.log('running scripts');
+    renderText();
+  });
 }(jQuery));
 
